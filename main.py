51290a3fbadf3e1517b149e4c5a1b7c2eba66917
thimi0412 <thimi0412@icloud.com>
2019-04-27 23:32:48 +0900

    update mp3ファイルをHOMEディレクトリから取得

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import subprocess
+import os
 from subprocess import Popen
 import threading
 import sys
 from pydub import AudioSegment
 from pydub.playback import play
+
+HOME_DIR = os.environ['HOME']
+MP3_1_DIR = HOME_DIR + '/mp3/build1.mp3'
+MP3_2_DIR = HOME_DIR + '/mp3/build2.mp3'
+MP3_3_DIR = HOME_DIR + '/mp3/build3.mp3'
+
 
 class Ridocker:
 
@@ -12,7 +19,7 @@ class Ridocker:
 
         #スレッドの作成と開始
         self.thread_1 = threading.Thread(target=self.exec)
-        self.thread_2 = threading.Thread(target=self.run_mp3, args=('mp3/build1.mp3',))
+        self.thread_2 = threading.Thread(target=self.run_mp3, args=(MP3_1_DIR,))
         self.thread_3 = threading.Thread(target=self.retry)
 
     def run_mp3(self, path):
@@ -26,14 +33,14 @@ class Ridocker:
         self.stop()
 
     def retry(self):
-        sound = AudioSegment.from_file('mp3/build2.mp3', 'mp3')
+        sound = AudioSegment.from_file(MP3_2_DIR, 'mp3')
         while not self.stop_event.is_set():
             play(sound)
 
     def stop(self):
         self.stop_event.set()
         self.thread_3.join()    #スレッドが停止するのを待つ
-        self.run_mp3('mp3/build3.mp3')
+        self.run_mp3(MP3_3_DIR)
 
 if __name__ == '__main__':
     h = Ridocker()
